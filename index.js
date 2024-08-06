5f76a3b970299d6b1eec278769acd57516ceb143
ankushKun <ankush4singh@gmail.com>
2024-08-06 14:40:42 +0530

    backend

---
+++ index.js
@@ -0,0 +1,151 @@
+import express from "express";
+import dockerode from "dockerode";
+import fs from "fs"
+import Irys from "@irys/sdk";
+
+const PORT = 3001;
+
+const app = express();
+app.use(express.json());
+
+
+async function deployFolder(path) {
+    console.log("Deploying folder at", path);
+
+    const jwk = JSON.parse(fs.readFileSync('./wallet.json', 'utf-8'));
+    const irys = new Irys({ url: 'https://turbo.ardrive.io', token: 'arweave', key: jwk });
+    irys.uploader.useChunking = false;
+
+
+
+    const txResult = await irys.uploadFolder(path, {
+        indexFile: 'index.html',
+        interactivePreflight: false,
+        logFunction: (log) => {
+            console.log(log);
+            fs.appendFileSync(`${path}/../log.txt`, log + '\n');
+        }
+    });
+
+    if (fs.existsSync(`${path}/../out-errors.txt`)) {
+        const errors = fs.readFileSync(`${path}/../out-errors.txt`, 'utf-8');
+        console.log('Errors:', errors);
+        fs.appendFileSync(`${path}/../log.txt`, errors + '\n');
+        throw new Error(errors);
+    } else {
+        console.log('No errors found');
+        return txResult.id;
+    }
+
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.post('/deploy', async (req, res) => {
+    console.log('Request:', req.body);
+    const { repository, installCommand, buildCommand, outputDirectory } = req.body;
+
+    if (!repository) return res.status(400).send('Repository is required');
+    if (!installCommand) return res.status(400).send('Install Command is required');
+    if (!buildCommand) return res.status(400).send('Build Command is required');
+    if (!outputDirectory) return res.status(400).send('Output Directory is required');
+
+    // 1. create a docker container
+    // 2. run an isolated build
+    // 3. move the output folder outside the container in ./builds folder
+    // 4. remove the container
+
+    const docker = new dockerode();
+
+    await docker.pull('node')
+    console.log('Pulled node image');
+
+    const container = await docker.createContainer({
+        Image: 'node',
+        Cmd: ['sh'],
+        AttachStdout: true,
+        AttachStderr: true,
+        Tty: true,
+        OpenStdin: true,
+        // Volumes: {
+        //     '/dumdumdeploy': {}
+        // },
+        // WorkingDir: '/dumdumdeploy',
+        HostConfig: {
+            Binds: [`${process.cwd()}/builds:/home/node/builds`]
+        }
+    })
+    console.log('Created container');
+    await container.start();
+
+    // remove trailing .git and / from repository url
+    const folderName = `${repository}`.replace(/\.git|\/$/, '').split('/').pop();
+    console.log('Folder name:', folderName);
+
+    var containerCommand = `cd /home/node;
+    rm -rf /home/node/${folderName}/${outputDirectory};
+    echo "" > /home/node/${folderName}/log.txt;
+    git clone ${repository} ${folderName};
+    cd /home/node/${folderName};
+    ${installCommand};
+    ${buildCommand};
+    mkdir /home/node/builds/${folderName};
+    cp -r /home/node/${folderName}/${outputDirectory} /home/node/builds/${folderName}`;
+
+    if (installCommand.startsWith('pnpm')) {
+        containerCommand = `npm i -g pnpm; ${containerCommand}`;
+    } else if (installCommand.startsWith('yarn')) {
+        containerCommand = `npm i -g yarn; ${containerCommand}`;
+    }
+
+    // create folder
+    fs.rmSync(`./builds/${folderName}`, { recursive: true, force: true });
+    fs.mkdirSync(`./builds/${folderName}`, { recursive: true });
+
+    const exec = await container.exec({
+        Cmd: ['sh', '-c', containerCommand],
+        AttachStderr: true,
+        AttachStdout: true,
+        Tty: true
+    });
+
+    exec.start({
+        hijack: true,
+        stdin: true,
+        Detach: false
+    }, (err, stream) => {
+        if (err) {
+            console.log('Exec error:', err);
+            return;
+        }
+
+        container.modem.demuxStream(stream, process.stdout, process.stderr);
+        // output to a file too
+        const fileStream = fs.createWriteStream(`./builds/${folderName}/log.txt`);
+        container.modem.demuxStream(stream, fileStream, fileStream);
+
+        stream.on('end', async (err) => {
+            console.log(err)
+            console.log('Exec end');
+            await container.commit();
+            if (!fs.existsSync(`./builds/${folderName}/${outputDirectory}/index.html`)) {
+                return res.status(500).send('index.html doesnot exist in build');
+            }
+            try {
+                const dres = await deployFolder(`./builds/${folderName}/${outputDirectory}`);
+                res.send(dres);
+            } catch (e) {
+                return res.status(400).send(e.message);
+            }
+
+            await container.stop();
+            await container.remove();
+        })
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+})
