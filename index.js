c6f8d0fb5023d346cd348f4d1943ad2670b8f752
ankushKun <ankush4singh@gmail.com>
2024-08-07 11:28:12 +0530

    timeout update

--- index.js
+++ index.js
@@ -168,5 +168,5 @@ app.get('/logs/:folder', (req, res) => {
 const server = app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
 })
-server.setTimeout(30 * 60 * 1000); // 30 minutes
-server.keepAliveTimeout = 30 * 60 * 1000; // 30 minutes
+server.setTimeout(60 * 60 * 1000);
+server.keepAliveTimeout = 60 * 60 * 1000;
